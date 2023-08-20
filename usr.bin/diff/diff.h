ab1149a3a12cf5def2cfb5685c81cec49c2aed1a
Andrew Markee <andrew.markee@gmail.com>
2023-08-20 12:44:48 -0500

    usr.bin/diff: Make -fno-common safe

--- usr.bin/diff/diff.h
+++ usr.bin/diff/diff.h
@@ -16,7 +16,7 @@
 /*
  * Output format options
  */
-int	opt;
+extern int	opt;
 
 #define	D_NORMAL	0	/* Normal output */
 #define	D_EDIT		-1	/* Editor script out */
@@ -26,49 +26,49 @@ int	opt;
 #define	D_NREVERSE	4	/* Reverse ed script with numbered
 				   lines and no trailing . */
 
-int	tflag;			/* expand tabs on output */
+extern int	tflag;			/* expand tabs on output */
 
 /*
  * Algorithm related options
  */
-int	hflag;			/* -h, use halfhearted DIFFH */
-int	bflag;			/* ignore blanks in comparisons */
-int	wflag;			/* totally ignore blanks in comparisons */
-int	iflag;			/* ignore case in comparisons */
+extern int	hflag;			/* -h, use halfhearted DIFFH */
+extern int	bflag;			/* ignore blanks in comparisons */
+extern int	wflag;			/* totally ignore blanks in comparisons */
+extern int	iflag;			/* ignore case in comparisons */
 
 /*
  * Options on hierarchical diffs.
  */
-int	lflag;			/* long output format with header */
-int	rflag;			/* recursively trace directories */
-int	sflag;			/* announce files which are same */
-char	*start;			/* do file only if name >= this */
+extern int	lflag;			/* long output format with header */
+extern int	rflag;			/* recursively trace directories */
+extern int	sflag;			/* announce files which are same */
+extern char	*start;			/* do file only if name >= this */
 
 /*
  * Variables for -I D_IFDEF option.
  */
-int	wantelses;		/* -E */
-char	*ifdef1;		/* String for -1 */
-char	*ifdef2;		/* String for -2 */
-char	*endifname;		/* What we will print on next #endif */
-int	inifdef;
+extern int	wantelses;		/* -E */
+extern char	*ifdef1;		/* String for -1 */
+extern char	*ifdef2;		/* String for -2 */
+extern char	*endifname;		/* What we will print on next #endif */
+extern int	inifdef;
 
 /*
  * Variables for -c context option.
  */
-int	context;		/* lines of context to be printed */
+extern int	context;		/* lines of context to be printed */
 
 /*
  * State for exit status.
  */
-int	status;
-int	anychange;
-char	*tempfile;		/* used when comparing against std input */
+extern int	status;
+extern int	anychange;
+extern char	*tempfile;		/* used when comparing against std input */
 
 /*
  * Variables for diffdir.
  */
-char	**diffargv;		/* option list to pass to recursive diffs */
+extern char	**diffargv;		/* option list to pass to recursive diffs */
 
 /*
  * Input file names.
@@ -76,8 +76,8 @@ char	**diffargv;		/* option list to pass to recursive diffs */
  * and padded with a '/', and then efile0 and efile1 point after
  * the '/'.
  */
-char	*file1, *file2, *efile1, *efile2;
-struct	stat stb1, stb2;
+extern char	*file1, *file2, *efile1, *efile2;
+extern struct	stat stb1, stb2;
 
 char	*talloc(), *ralloc();
 char	*savestr(), *splice(), *splicen();
